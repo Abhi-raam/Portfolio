3a0724a1a2a3183a88d339ce8b6bf34e0d7c22ae
Abhiram Suresh <abhiramunni246@gmail.com>
2023-10-25 00:10:54 +0530

    fet : project bg changed to gray-900

--- src/Components/ProjectCards.jsx
+++ src/Components/ProjectCards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ProjectCards({ project }) {
     const { Title, Description, Tag,Image } = { ...project }
     return (
-        <div className="group card card-side hover:shadow-2xl overflow-hidden cursor-pointer sm:h-[20rem] bg-slate-300 w-auto md:w-[40rem] justify-self-center shadow-xl">
+        <div className="group card card-side overflow-hidden cursor-pointer sm:h-[20rem] bg-slate-300 w-auto md:w-[40rem] justify-self-center shadow-xl">
             <div className='bg-slate-500'>
                 <img src={Image} className='absolute  hidden w-[35rem] md:w-[33rem]  sm:block top-9 -right-[10rem]  rounded-t-lg shadow-2xl  group-hover:-translate-x-3 group-hover:scale-[1.04] group-hover:-translate-y-3 group-hover:-rotate-2 transition' alt="" />
                 {/* <img src={Image} className='absolute  hidden  sm:block top-9 -right-[18rem] w-[36.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-[18rem] group-hover:-translate-x-3 group-hover:scale-[1.04] group-hover:-translate-y-3 group-hover:-rotate-2 transition group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-3' alt="" /> */}
