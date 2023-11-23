d5e370abb1868928296145862b83459ed747341b
Abhiram Suresh <abhiramunni246@gmail.com>
2023-11-24 04:15:50 +0530

    making pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA  } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
+const manifestForPlugin = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+	manifest: {
+    "short_name": "Abhiram",
+    "name": "Abhiram",
+    "icons": [
+      {
+        "src": "favicon.ico",
+        "sizes": "64x64 32x32 24x24 16x16",
+        "type": "image/x-icon"
+      },
+      {
+        "src": "logo192.png",
+        "type": "image/png",
+        "sizes": "192x192"
+      },
+      {
+        "src": "logo256.png",
+        "type": "image/png",
+        "sizes": "256x256"
+      },
+      {
+        "src": "logo384.png",
+        "type": "image/png",
+        "sizes": "384x384"
+      },
+      {
+        "src": "logo512.png",
+        "type": "image/png",
+        "sizes": "512x512"
+      }
+    ],
+    "theme_color": "#000000",
+    "background_color": "#000000",
+    "display": "standalone",
+    "scope": "/",
+    "start_url": "/",
+  }
+};
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0'
-  },
-})
+  base: "./",
+  plugins: [react(),VitePWA(manifestForPlugin)]
+});
