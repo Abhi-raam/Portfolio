489fd7f3aaecf6c262501965b0a6ac4aaf236403
Abhiram Suresh <abhiramunni246@gmail.com>
2023-11-26 20:09:56 +0530

    responsive error in about section fixed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,12 +6,12 @@ import {AboutData , Links} from '../Data/AboutData'
 function About() {
   return (
     <div id="about" className=" sm:flex justify-evenly p-3 pt-16 items-center   bg-slate-900">
-      <div className="grid  gap-9 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:max-w-5xl">
+      <div className="grid  gap-9 grid-cols-1 md:grid-cols-3 xl:grid-cols-4 sm:max-w-5xl">
         <div data-aos="fade-up" data-aos-duration="1000"data-aos-delay="300"  className="  font-semibold text-2xl sm:text-5xl">
           <h1 className="text-slate-200 text-center md:text-start">ABOUT<span className="text-yellow-400 ml-2">ME</span>
           </h1>
         </div>
-        <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" className="col-span-1 lg:col-span-2 ">
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" className="col-span-1 lg:col-span-1 xl:col-span-2 ">
           {AboutData.map((about, index) => (
             <div key={index} className="text-slate-200">
               <h1 className="font-semibold text-center text-3xl">{about.Title}</h1>
